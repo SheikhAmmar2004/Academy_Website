9eb34e49a02c3c15f74f370de70e9d656f4a5c0d
Sheikh Ammar <ammar.sheikh1230@gmail.com>
2025-10-03 19:53:55 +0500

    Fixed sitemap location

--- LearnandGrow/academy/urls.py
+++ LearnandGrow/academy/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("submit-trial/", views.submit_trial, name="submit_trial"),
     path("submit-enrollment/", views.submit_enrollment, name="submit_enrollment"),
     path("submit-feedback/", views.submit_feedback, name="submit_feedback"),
-
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
 ]
